53a4d15f6f779164751a3cd318dfd346f4f45ac4
GalacticKittenSS <daniel05james@gmail.com>
2021-11-21 16:30:28 +0000

    Updated Premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,9 @@
 project "ImGui"
 	kind "StaticLib"
+    staticruntime "on"
+
 	language "C++"
-    staticruntime "off"
-	
-	warnings "off"
+    cppdialect "C++17"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
@@ -27,21 +27,11 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
